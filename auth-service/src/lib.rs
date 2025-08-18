780273bc06ce0dc642aba44c98056a592cc8c5ef
Gary Smith <scopevale@gmail.com>
2025-08-18 23:24:18 +0100

    [Sprint 1][Task 5] feat: implement routes using TDD

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,4 +1,10 @@
-use axum::{serve::Serve, Router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::post,
+    serve::Serve,
+    Router
+};
 use tower_http::services::ServeDir;
 use std::error::Error;
 
@@ -12,11 +18,13 @@ pub struct Application {
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        // Move the Router definition from `main.rs` to here.
-        // Also, remove the `hello` route.
-        // We don't need it at this point!
         let router = Router::new()
-        .nest_service("/", ServeDir::new("assets"));
+        .nest_service("/", ServeDir::new("assets"))
+        .route("/signup", post(signup))
+        .route("/login", post(login))
+        .route("/logout", post(logout))
+        .route("/verify-2fa", post(verify_2fa))
+        .route("/verify-token", post(verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -30,4 +38,24 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
