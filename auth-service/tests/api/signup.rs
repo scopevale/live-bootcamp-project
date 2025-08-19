10f4d6579dae3d2e1b8ad203efd3517d2cc5aeb0
Gary Smith <scopevale@gmail.com>
2025-08-19 23:00:13 +0100

    [Sprint 1][Task 6] chore: update integration tests module structure

---
+++ auth-service/tests/api/signup.rs
@@ -0,0 +1,10 @@
+use crate::helpers::TestApp;
+
+// Tokio's test macro is used to run the test in an async environment
+#[tokio::test]
+async fn signup_returns_auth_ui() {
+    let app = TestApp::new().await;
+    let response = app.post_signup().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
