c3ebeb26926c51fdcfc71e9442c02083dd2aeebc
Gary Smith <scopevale@gmail.com>
2025-08-30 10:11:06 +0100

    [Sprint 2][Task 2] test: add 400 & 409 tests (failing)

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,5 @@
 use auth_service::routes::SignupResponse;
+use axum::response::ErrorResponse;
 
 use crate::helpers::{get_random_email, TestApp};
 
@@ -72,6 +73,72 @@ async fn should_return_201_if_valid_input() {
         .expect("Failed to parse response body."),
         expected_response
     );
-
-
 }
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    // The signup route should return a 400 HTTP status code if an invalid input is sent.
+    // The input is considered invalid if:
+    // - The email is empty or does not contain '@'
+    // - The password is less than 8 characters
+
+    // Create an array of invalid inputs. Then, iterate through the array and
+    // make HTTP calls to the signup route. Assert a 400 HTTP status code is returned.
+    todo!();
+    let _app = TestApp::new().await;
+
+    let random_email = get_random_email(); // Call helper method to generate email
+
+    // add some malformed input test cases
+    let _test_cases = [
+        serde_json::json!({             // empty email field
+            "email": "",
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({             // empty password field
+            "email": random_email,
+            "password": "",
+            "requires2FA": false
+        }),
+        serde_json::json!({             // empty email & password fields
+            "email": "",
+            "password": "",
+            "requires2FA": false
+        }),
+        serde_json::json!({             // invalid email
+            "email": "random_email",
+            "password": "password123",
+            "requires2FA": false
+        }),
+        serde_json::json!({             // invalid password
+            "email": random_email,
+            "password": "short",
+            "requires2FA": true
+        }),
+        serde_json::json!({             // empty JSON object
+        }),
+    ];
+
+    // for test_case in test_cases.iter() {
+    //     // call `post_signup`
+    //     let response = app.post_signup(test_case).await;
+    //     assert_eq!(response.status().as_u16(), 400, "Failed for input: {:?}", test_case);
+    //
+    //     assert_eq!(
+    //         response
+    //             .json::<ErrorResponse>()
+    //             .await
+    //             .expect("Failed to deserialize response body to ErrorResponse")
+    //             .error,
+    //         "Invalid credentials".to_owned()
+    //     );
+    // }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    // Call the signup route twice. The second request should fail with a 409 HTTP status code
+    todo!()
+}
+
