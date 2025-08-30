9c5b6a6ff80a5924e92a3be1756593973e373aa5
Gary Smith <scopevale@gmail.com>
2025-08-30 10:38:52 +0100

    [Sprint 2][Task 2] test: update 400 & 409 test cases

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,4 +1,4 @@
-use auth_service::routes::SignupResponse;
+use auth_service::{routes::SignupResponse, ErrorResponse};
 
 use crate::helpers::{get_random_email, TestApp};
 
@@ -83,13 +83,12 @@ async fn should_return_400_if_invalid_input() {
 
     // Create an array of invalid inputs. Then, iterate through the array and
     // make HTTP calls to the signup route. Assert a 400 HTTP status code is returned.
-    todo!();
-    let _app = TestApp::new().await;
+    let app = TestApp::new().await;
 
     let random_email = get_random_email(); // Call helper method to generate email
 
     // add some malformed input test cases
-    let _test_cases = [
+    let test_cases = [
         serde_json::json!({             // empty email field
             "email": "",
             "password": "password123",
@@ -119,25 +118,50 @@ async fn should_return_400_if_invalid_input() {
         }),
     ];
 
-    // for test_case in test_cases.iter() {
-    //     // call `post_signup`
-    //     let response = app.post_signup(test_case).await;
-    //     assert_eq!(response.status().as_u16(), 400, "Failed for input: {:?}", test_case);
-    //
-    //     assert_eq!(
-    //         response
-    //             .json::<ErrorResponse>()
-    //             .await
-    //             .expect("Failed to deserialize response body to ErrorResponse")
-    //             .error,
-    //         "Invalid credentials".to_owned()
-    //     );
-    // }
+    for test_case in test_cases.iter() {
+        // call `post_signup`
+        let response = app.post_signup(test_case).await;
+        assert_eq!(response.status().as_u16(), 400, "Failed for input: {:?}", test_case);
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
 }
 
 #[tokio::test]
 async fn should_return_409_if_email_already_exists() {
     // Call the signup route twice. The second request should fail with a 409 HTTP status code
-    todo!()
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email(); // Call helper method to generate email
+
+    let test_case =
+      serde_json::json!({
+          "email": random_email,
+          "password": "password123",
+          "requires2FA": true
+    });
+
+    // call `post_signup` the first time
+    let mut response = app.post_signup(&test_case).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    // call `post_signup` the second time
+    response = app.post_signup(&test_case).await;
+    assert_eq!(response.status().as_u16(), 409);
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "User already exists".to_owned()
+    );
 }
 
