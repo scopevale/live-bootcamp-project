c0a6cacfc0bec54690fd9686c9d079f1930af2ee
Gary Smith <scopevale@gmail.com>
2025-09-16 12:55:54 +0100

    [Sprint 4][Task 1] test: implement 2fa store code trait

--- auth-service/src/domain/data_stores.rs
+++ auth-service/src/domain/data_stores.rs
@@ -27,3 +27,85 @@ pub trait BannedTokenStore: std::fmt::Debug {
 pub enum BannedTokenStoreError {
     UnexpectedError,
 }
+
+// This trait represents the interface all concrete 2FA code stores should implement
+#[async_trait::async_trait]
+pub trait TwoFACodeStore {
+    async fn add_code(
+        &mut self,
+        email: Email,
+        login_attempt_id: LoginAttemptId,
+        code: TwoFACode,
+    ) -> Result<(), TwoFACodeStoreError>;
+    async fn remove_code(&mut self, email: &Email) -> Result<(), TwoFACodeStoreError>;
+    async fn get_code(
+        &self,
+        email: &Email,
+    ) -> Result<(LoginAttemptId, TwoFACode), TwoFACodeStoreError>;
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TwoFACodeStoreError {
+    LoginAttemptIdNotFound,
+    UnexpectedError,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LoginAttemptId(String);
+
+impl LoginAttemptId {
+    pub fn parse(id: String) -> Result<Self, String> {
+        // Use the `parse_str` function from the `uuid` crate to ensure `id` is a valid UUID
+        match uuid::Uuid::parse_str(&id) {
+            Ok(_) => Ok(Self(id)),
+            Err(_) => Err("Invalid login attempt id".to_owned()),
+        }
+    }
+}
+
+impl Default for LoginAttemptId {
+    fn default() -> Self {
+        // Use the `uuid` crate to generate a random version 4 UUID
+        Self(uuid::Uuid::new_v4().to_string())
+    }
+}
+
+// Implement AsRef<str> for LoginAttemptId
+impl AsRef<str> for LoginAttemptId {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct TwoFACode(String);
+
+impl TwoFACode {
+    pub fn parse(code: String) -> Result<Self, String> {
+        // Ensure `code` is a valid 6-digit code
+        if code.len() == 6 && code.chars().all(|c| c.is_ascii_digit()) {
+            Ok(Self(code))
+        } else {
+            Err("Invalid 2FA code".to_owned())
+        }
+    }
+}
+
+impl Default for TwoFACode {
+    fn default() -> Self {
+        // Use the `rand` crate to generate a random 2FA code.
+        // The code should be 6 digits (ex: 834629)
+        let code: String = (0..6)
+            .map(|_| rand::random::<u8>() % 10) // Generate a random digit (0-9)
+            .map(|digit| char::from(b'0' + digit)) // Convert digit to char
+            .collect();
+        Self(code)
+    }
+}
+
+// Implement AsRef<str> for TwoFACode
+impl AsRef<str> for TwoFACode {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
