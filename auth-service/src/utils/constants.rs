12496188795ff206d89f594141a74adc22a434cb
Gary Smith <scopevale@gmail.com>
2025-09-17 19:37:58 +0100

    [Sprint 5][Task 1] chore: setup PostgreSQL docker container

--- auth-service/src/utils/constants.rs
+++ auth-service/src/utils/constants.rs
@@ -5,6 +5,7 @@ use std::env as std_env;
 // Define a lazily evaluated static. lazy_static is needed because std_env::var is not a const function.
 lazy_static! {
     pub static ref JWT_SECRET: String = set_token();
+    pub static ref DATABASE_URL: String = set_dburl();
 }
 
 fn set_token() -> String {
@@ -23,8 +24,20 @@ fn set_token() -> String {
     secret
 }
 
+fn set_dburl() -> String {
+    dotenv().ok(); // Load environment variables
+    let dburl = std_env::var(env::DATABASE_URL_ENV_VAR).unwrap_or_else(|_| {
+        panic!("DATABASE_URL must be set.");
+    });
+    if dburl.is_empty() {
+        panic!("DATABASE_URL must not be empty.");
+    }
+    dburl
+}
+
 pub mod env {
     pub const JWT_SECRET_ENV_VAR: &str = "JWT_SECRET";
+    pub const DATABASE_URL_ENV_VAR: &str = "DATABASE_URL";
 }
 
 pub mod prod {
