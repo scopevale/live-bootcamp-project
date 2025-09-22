dc23945a0013dd7b5f9cd0f63e421b1fbf93c2f5
Gary Smith <scopevale@gmail.com>
2025-09-22 12:25:52 +0100

    [Sprint 5][Task 2] feat: add PostgreSQL user store

--- auth-service/src/services/data_stores.rs
+++ auth-service/src/services/data_stores.rs
@@ -1,9 +1,11 @@
 pub mod hashmap_two_fa_code_store;
 pub mod hashmap_user_store;
 pub mod hashset_banned_token_store;
+pub mod postgres_user_store;
 pub mod user_stores;
 
 pub use hashmap_two_fa_code_store::*;
 pub use hashmap_user_store::*;
 pub use hashset_banned_token_store::*;
+pub use postgres_user_store::*;
 pub use user_stores::*;
